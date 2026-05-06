15cf5c9176cf1d91d0403d3c8770c3e2e1cadb18
KungaNyimaGurung <mrrapxang@gmail.com>
2026-05-06 18:21:14 -0400

    MongoDB and Docker connected successfully

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import app from './app.js';
+import connectDB from './config/db.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
